f125ad80ab0932fc75bf24c1cc8bc74d98766dfa
lociX55Lim@gmail.com <lociX55Lim@gmail.com>
2023-07-18 13:58:28 +0300

    feat: done urlshort task

---
+++ urlshort/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"main.go/handler"
+	"net/http"
+)
+
+func main() {
+	mux := defaultMux()
+
+	// Build the MapHandler using the mux as the fallback
+	pathsToUrls := map[string]string{
+		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
+		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
+	}
+	mapHandler := handler.MapHandler(pathsToUrls, mux)
+	// Build the YAMLHandler using the mapHandler as the
+	// fallback
+	yaml := `
+- path: /urlshort
+  url: https://github.com/gophercises/urlshort
+- path: /urlshort-final
+  url: https://github.com/gophercises/urlshort/tree/solution
+`
+	yamlHandler, err := handler.YAMLHandler([]byte(yaml), mapHandler)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Starting the server on :8080")
+	err = http.ListenAndServe(":8080", yamlHandler)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func defaultMux() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", hello)
+	return mux
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, world!")
+}
